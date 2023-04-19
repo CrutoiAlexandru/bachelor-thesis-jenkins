86da2f081a0aba25297aa739c99cb8106bc38ca9
Alexandru-Vitali.Crutoi.76010082 <Alexandru-Vitali.Crutoi@microfocus.com>
2023-04-19 22:00:30 +0300

    reformat total-shutdown.sh

--- scripts/total-shutdown.sh
+++ scripts/total-shutdown.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Get the instance IDs and names
-instance_info=$(aws ec2 describe-instances --region eu-central-1 --query 'Reservations[].Instances[].[InstanceId, Tags[?Key==`Name`].Value|[0]]' --output text)
+# Get the instance IDs
+instance_ids=$(aws ec2 describe-instances --region eu-central-1 --query 'Reservations[].Instances[].InstanceId' --output text)
 
-# Loop through each instance ID and stop the instance
-while read instance_id instance_name; do
-    echo "Stopping instance $instance_name ($instance_id)..."
-    aws ec2 stop-instances --region eu-central-1 --instance-ids $instance_id
-done <<<"$instance_info"
+# Stop the instances
+aws ec2 stop-instances --region eu-central-1 --instance-ids $instance_ids
