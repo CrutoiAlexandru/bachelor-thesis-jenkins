997c2b2aec171d1adbdb9252d3070b5ee8624a57
Alexandru-Vitali.Crutoi.76010082 <Alexandru-Vitali.Crutoi@microfocus.com>
2023-04-20 13:23:29 +0300

    create shutdown.sh and startup.sh

---
+++ scripts/shutdown.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get the instance ID
+INSTANCE_ID=$(aws ec2 describe-instances --region eu-central-1 --filters "Name=tag:Name,Values=$INSTANCE_NAME" --query "Reservations[].Instances[].InstanceId" --output text)
+
+# Check if instance ID is empty
+if [ -z "$INSTANCE_ID" ]; then
+    echo "Error: No instance found with name $INSTANCE_NAME"
+    exit 1
+fi
+
+# Stop the instance
+aws ec2 stop-instances --region eu-central-1 --instance-ids "$INSTANCE_ID"
+
+# Print success message
+echo "Instance $INSTANCE_NAME ($INSTANCE_ID) is stopping."
