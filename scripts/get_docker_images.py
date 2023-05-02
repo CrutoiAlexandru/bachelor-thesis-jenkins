629b926f3a40951303fa12a011792befe5734249
Alexandru-Vitali.Crutoi.76010082 <Alexandru-Vitali.Crutoi@microfocus.com>
2023-05-02 15:50:00 +0300

    fix

--- scripts/get_docker_images.py
+++ scripts/get_docker_images.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 def get_versions(repository):
-    cmd = f'docker search "{repository}"'
+    cmd = f'sudo docker search "{repository}"'
     output = subprocess.check_output(cmd, shell=True)
     lines = output.decode('utf-8').splitlines()
     versions = [line.split(':')[1] for line in lines]
