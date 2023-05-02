00d0848afe02316b2c1661e21af8402b8f9c6336
Alexandru-Vitali.Crutoi.76010082 <Alexandru-Vitali.Crutoi@microfocus.com>
2023-05-02 15:13:25 +0300

    create index.html

--- scripts/get_docker_images.py
+++ scripts/get_docker_images.py
@@ -5,5 +5,5 @@ def get_versions(repository):
     cmd = f'docker search "{repository}"'
     output = subprocess.check_output(cmd, shell=True)
     lines = output.decode('utf-8').splitlines()
-    versions = [line.split(':') for line in lines]
+    versions = [line.split(':')[1] for line in lines]
     return versions
--- scripts/update-web-server.py
+++ scripts/update-web-server.py
@@ -21,32 +21,87 @@ cnx = mysql.connector.connect(user=rds_user, password=rds_password,
 cursor = cnx.cursor()
 
 repository = 'crutoialexandru/flask-file-hosting'
+product_name = 'flaskfilehosting'
 versions = get_versions(repository)
+tags = {}
 
 for version in versions:
 
-    select_stmt = f"SELECT * FROM {rds_table} WHERE product = '{product_name}' ORDER BY build_number DESC LIMIT 1"
+    select_stmt = f"SELECT tag FROM {rds_table} WHERE product = '{product_name}' AND build_number = '{version}'"
     cursor.execute(select_stmt)
 
-    response = cursor.fetchone()
+    tags[version] = cursor.fetchone()
 
-content = f"""
-<table>
-<tr>
-    <th>Version</th>
-    <th>Tag</th>
-</tr>
+# header
+content = """
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>FlaskFileHosting</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <style>
+      body {
+        background-color: #f0f0f0;
+      }
+      .jumbotron {
+        background-color: #00abe7;
+        color: #ffffff;
+      }
+    </style>
+  </head>
+  <body>
+"""
+
+# body
+content += """
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">FlaskFileHosting</h1>
+        <p class="lead">A file sharing Docker image application</p>
+      </div>
+    </div>
+
+    <div class="container">
+      <h2>Download</h2>
+      <p>Here are the available versions and tags:</p>
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th>Version</th>
+            <th>Tag</th>
+          </tr>
+        </thead>
+        <tbody>
 """
 
 for version in versions:
     content += f"""
+        <tr>
+        <td>{version}</td>
+        <td><a href="#">latest</a></td>
+        </tr>
     <tr>
         <td>{version}</td>
-        <td>{versions[version]}</td>
+        <td>{tags[version]}</td>
     </tr>
     """
 
-content += "</table>"
+content += """
+        </tbody>
+      </table>
+    </div>
+"""
+
+# end
+content += """
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  </body>
+</html>
+"""
 
 with open('/server/index.html') as f:
     f.write(content)
