4aa642bb984441276bf8c223292b85a7a76eef04
Alexandru-Vitali.Crutoi.76010082 <Alexandru-Vitali.Crutoi@microfocus.com>
2023-05-02 15:58:58 +0300

    fix

--- scripts/get_docker_images.py
+++ scripts/get_docker_images.py
@@ -5,5 +5,8 @@ def get_versions(repository):
     cmd = f'sudo docker search "{repository}"'
     output = subprocess.check_output(cmd, shell=True)
     lines = output.decode('utf-8').splitlines()
-    versions = [line.split(':')[1] for line in lines]
+    try:
+        versions = [line.split(':')[1] for line in lines]
+    except IndexError:
+        versions = []
     return versions
